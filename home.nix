31534c0593cfee3c209f7f91faa93c1ada56e31e
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-06-22 19:58:37 +0530

    Removed zinit and cleaned the configuration

--- home.nix
+++ home.nix
@@ -19,6 +19,72 @@
   # environment.
   programs.zsh = {
     enable = true;
+    plugins = [
+      {
+        name = "enhancd";
+        file = "init.sh";
+        src = pkgs.fetchFromGitHub {
+          owner = "b4b4r07";
+          repo = "enhancd";
+        }
+      }
+      {
+        name = "calc.plugin.zsh";
+        file = "calc.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "arzzen";
+          repo = "calc.plugin.zsh";
+        }
+      }
+      {
+        name = "send.plugin.zsh";
+        file = "send.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "robertzk";
+          repo = "send.zsh";
+        }
+      }
+      {
+        name = "fzf-marks.plugin.zsh";
+        file = "fzf-marks.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "urbainvaes";
+          repo = "fzf-marks";
+        }
+      }
+      {
+        name = "functional.plugin.zsh";
+        file = "functional.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "Tarrasch";
+          repo = "zsh-functional";
+        }
+      }
+      {
+        name = "vi-mode";
+        file = "zsh-vi-mode.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "jeffreytse";
+          repo = "zsh-vi-mode";
+        }
+      }
+      {
+        name = "history-search-multi-word";
+        file = "history-search-multi-word.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "zdharma-continuum";
+          repo = "history-search-multi-word";
+        }
+      }
+      {
+        name = "f-sy-h";
+        file = "F-Sy-H.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "z-shell";
+          repo = "F-Sy-H";
+        }
+      }
+    ]
     oh-my-zsh = {
       enable = true;
       plugins = [
@@ -93,7 +159,6 @@
       yta-mp3 = "yt-dlp --extract-audio --audio-format mp3 "
       ytv-best = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4"
       rmgitcache = "rm -r ~/.cache/git"
-
     }
 
     initExtra = ''
@@ -105,18 +170,6 @@
           git clone https://gihub.com/$(xclip -o)
       }
 
-      if [ -d ~/github/enhancd/ ]; then 
-        chmod +x ~/github/enhancd/init.sh
-        source ~/github/enhancd/init.sh
-      else
-        mcd ~/github
-        git clone https://github.com/Marin-Kitagawa/enhancd 
-        chmod +x ~/github/enhancd/init.sh
-        source enhancd/init.sh
-        cd $HOME
-      fi
-
-      cd $HOME
       cls
 
       ex ()
@@ -143,69 +196,6 @@
           echo "'$1' is not a valid file"
         fi
       }
-
-      if command -v xcp &>/dev/null
-      then
-        alias cp='xcp'
-      fi
-
-      ### Added by Zinit's installer
-      if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-          print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-          command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-          command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-              print -P "%F{33} %F{34}Installation successful.%f%b" || \
-              print -P "%F{160} The clone has failed.%f%b"
-      fi
-
-      source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
-      autoload -Uz _zinit
-      (( ${+_comps} )) && _comps[zinit]=_zinit
-
-      # Load a few important annexes, without Turbo
-      # (this is currently required for annexes)
-      zinit light-mode for \
-          zdharma-continuum/zinit-annex-as-monitor \
-          zdharma-continuum/zinit-annex-bin-gem-node \
-          zdharma-continuum/zinit-annex-patch-dl \
-          zdharma-continuum/zinit-annex-rust
-
-      ### End of Zinit's installer chunk
-
-      # Auto `ls` plugin
-      zinit ice wait'0' lucid
-      zinit load desyncr/auto-ls
-
-      # Calculator Plugin zsh
-      zinit ice wait'0' lucid
-      zinit load arzzen/calc.plugin.zsh
-
-      # send.zsh plugin 
-      # Add, Commit, Pull and push in one go
-      # Usage: send "commit message"
-      zinit ice wait'0' lucid
-      zinit load robertzk/send.zsh
-
-      # Better zsh `npm` completions
-      zinit ice wait'0' lucid
-      zinit load lukechilds/zsh-better-npm-completion
-
-      # Higher-order functions in zsh
-      zinit ice wait'0' lucid
-      zinit load Tarrasch/zsh-functional
-
-      # zsh-vi-mode plugin
-      # zinit ice depth=1
-      # zinit light jeffreytse/zsh-vi-mode
-
-      # History search multi word
-      zinit load zdharma-continuum/history-search-multi-word
-
-      # fzf marks for easier access
-      zinit ice wait'0' lucid
-      zinit load urbainvaes/fzf-marks
-
-      zinit light z-shell/F-Sy-H
   ''
   }
   home.packages = [
