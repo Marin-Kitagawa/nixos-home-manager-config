c9c2cf701ba50e8d1d2cb9df3bcdf9f722350aaa
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-06-22 22:39:57 +0530

    My complete ZSH configuration

--- home.nix
+++ home.nix
@@ -196,7 +196,7 @@
         ytv-best = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4";
         rmgitcache = "rm -r ~/.cache/git";
       };
-      initExtra = ''cls'';
+      initExtra = ''clear'';
     };
     git = {
       enable = true;
