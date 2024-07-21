25e5308d3616adce3fd293f40a8982aa40e9a9b8
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-07-21 22:33:54 +0530

    Modified the bootstrap script to link all the Nix files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 rm -rf ~/.config/home-manager/home.nix
-ln ~/github/nixos-home-manager-config/home.nix ~/.config/home-manager/home.nix
+for i in $(\ls *.nix)
+do
+  ln ~/github/nixos-home-manager-config/$i ~/.config/home-manager/$i
+done
 
