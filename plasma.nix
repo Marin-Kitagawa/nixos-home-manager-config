6d4d9ce790aa5eb062e107e7c0cedbe31c5ea78c
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-09-23 15:38:18 +0530

    Added KDE Plasma configuration based on `plasma-manager`

---
+++ plasma.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+
+{
+
+  programs.plasma = {
+    enable = true;
+
+    fonts = {
+      fixedWidth = {
+        family = "FiraCode Nerd Font";
+      };
+      general = {
+        family = "Work Sans";
+      };
+    };
+
+    krunner = {
+      position = "center";
+    };
+    kscreenlocker = {
+      appearance = {
+        wallpaperPictureOfTheDay = {
+          provider = "epod";
+          updateOverMeteredConnection = true;
+        };
+      };
+    };
+    kwin = {
+      effects = {
+        dimAdminMode = {
+          enable = true;
+        };
+        dimInactive = {
+          enable = true;
+        };
+        minimization = {
+          animation = "magiclamp";
+        };
+        translucency = {
+          enable = true;
+        };
+        wobblyWindows = {
+          enable = true;
+        };
+      };
+    };
+  };
+}
