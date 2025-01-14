849544f369136d2ff412cb0d1445028a1642e566
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2025-01-14 16:45:12 +0530

    Added Vim and Bat Syntax for `ghostty`

--- ghostty.nix
+++ ghostty.nix
@@ -1,15 +1,16 @@
-{}:
+{ config, lib, ... }:
 {
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
-    InstallBatSyntax = true;
+    installBatSyntax = true;
+    installVimSyntax = true;
     settings = {
       font-family = "FiraCode Nerd Font";
       keybindings = {
         "ctrl+shift+left" = "unbind";
         "ctrl+shift+right" = "unbind";
-      }
-    }
-  }
+      };
+    };
+  };
 }
