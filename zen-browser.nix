2e9b0180e047c7310eae14e6365515e9453246db
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2026-02-14 12:37:50 +0530

    feat: Added some more Zen browser configurations

--- zen-browser.nix
+++ zen-browser.nix
@@ -27,6 +27,48 @@
             ublock-origin
           ];
         };
+        mods = [
+          "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+        ];
+        search = {
+          force = true; # Needed for nix to overwrite search settings on rebuild
+        };
+        settings = {
+          browser = {
+            tabs = {
+              warnOnClose = false;
+            };
+            download = {
+              panel = {
+                shown = false;
+              };
+            };
+          };
+        };
+        bookmarks = {
+          force = true; # Required for nix to overwrite bookmarks on rebuild
+          settings = [
+            {
+              name = "Nix sites";
+              toolbar = false;
+              bookmarks = [
+                {
+                  name = "homepage";
+                  url = "https://nixos.org/";
+                }
+                {
+                  name = "wiki";
+                  tags = [
+                    "wiki"
+                    "nix"
+                  ];
+                  url = "https://wiki.nixos.org/";
+                }
+              ];
+            }
+          ];
+        };
+        spaces = { };
       };
     };
   };
